591062ba285626edef69e1ab31c4633a77b537d1
Rinya HIRAYAMA <e205709@ie.u-ryukyu.ac.jp>
2020-12-17 20:58:52 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/ie/e205709/Report5.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e205709;
+
+public class Report5 {
+    
+    public static void main(String[] args){
+
+        String str = null;
+        int num = str.length();
+    
+        System.out.println(num);
+
+    }
+}
