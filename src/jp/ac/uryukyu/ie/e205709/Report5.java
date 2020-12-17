5d0979fe54216773d57e809f2d38cc0f886c51c0
Rinya HIRAYAMA <e205709@ie.u-ryukyu.ac.jp>
2020-12-17 22:33:12 +0900

    done for Step2

--- src/jp/ac/uryukyu/ie/e205709/Report5.java
+++ src/jp/ac/uryukyu/ie/e205709/Report5.java
@@ -1,13 +1,25 @@
 package jp.ac.uryukyu.ie.e205709;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Report5 {
     
     public static void main(String[] args){
-
-        String str = null;
-        int num = str.length();
     
-        System.out.println(num);
+        try{
+            // FileWriter fw = new FileWriter("output.txt");
+            String str = null;
+            System.out.println(str.length());
+           
+            // fw.close();
+
+        }catch  (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生");
+
+        // }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
